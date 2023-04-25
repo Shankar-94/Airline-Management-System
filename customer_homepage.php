0ecb2efc744e62e96a35081a3a1b2ebaccb0f294
Shankar-94 <121161887+Shankar-94@users.noreply.github.com>
2023-04-26 00:55:23 +0530

    Airline

---
+++ customer_homepage.php
@@ -0,0 +1,176 @@
+<?php
+	session_start();
+	// if($_SESSION['login_user']==null){
+	// 	header('location:home_page.php');
+	// }
+?>
+<html>
+	<head>
+		<title>
+			Welcome Customer
+		</title>
+		<style>
+			input {
+    			border: 1.5px solid #030337;
+    			border-radius: 4px;
+    			padding: 7px 30px;
+			}
+			input[type=submit] {
+				background-color: #030337;
+				color: white;
+    			border-radius: 4px;
+    			padding: 7px 45px;
+    			margin: 0px 127px
+			}
+			input[type=date] {
+				border: 1.5px solid #030337;
+    			border-radius: 4px;
+    			padding: 5.5px 44.5px;
+			}
+			select {
+    			border: 1.5px solid #030337;
+    			border-radius: 4px;
+    			padding: 6.5px 75.5px;
+			}
+		</style>
+		<link rel="stylesheet" type="text/css" href="css/style.css"/>
+		<link rel="stylesheet" href="font-awesome-4.7.0\css\font-awesome.min.css">
+	</head>
+	<body>
+		<center>
+			<img class="logo" src="images/shutterstock_22.jpg"/> 
+			<h1 id="title">
+				GOGTE BCA AIRLINES
+			</h1>
+		</center>
+		<div>
+			<ul>
+				<li><a href="customer_homepage.php"><i class="fa fa-home" aria-hidden="true"></i> Home</a></li>
+				<li><a href="customer_homepage.php"><i class="fa fa-desktop" aria-hidden="true"></i> Dashboard</a></li>
+				<li><a href="home_page.php"><i class="fa fa-plane" aria-hidden="true"></i> About Us</a></li>
+				<li><a href="home_page.php"><i class="fa fa-phone" aria-hidden="true"></i> Contact Us</a></li>
+				<li><a href="logout_handler.php"><i class="fa fa-sign-out" aria-hidden="true"></i> Logout</a></li>
+			</ul>
+		</div>
+		
+		<div class="container">
+		<div class="row">
+		<section class="col-4">
+		<?php
+			echo "<h2>Welcome ".$_SESSION['login_user']."</h2>";
+		?>
+		<table cellpadding="5">
+			<tr>
+				<td class="admin_func"><a href="book_tickets.php"><i class="fa fa-plane" aria-hidden="true"></i> Book Flight Tickets</a>
+				</td>
+			</tr>
+			<tr>
+				<td class="admin_func"><a href="view_booked_tickets.php"><i class="fa fa-plane" aria-hidden="true"></i> View Booked Flight Tickets</a>
+				</td>
+			</tr>
+			<tr>
+				<td class="admin_func"><a href="cancel_booked_tickets.php"><i class="fa fa-plane" aria-hidden="true"></i> Cancel Booked Flight Tickets</a>
+				</td>
+			</tr>
+		</table>
+		</section>
+		
+		<section class="col-8">
+		<h3 class='set_nice_size'><center><u>Upcoming Trips</u></center></h3>
+		<?php
+			$todays_date=date('Y-m-d');
+			$thirty_days_before_date=date_create(date('Y-m-d'));
+			date_sub($thirty_days_before_date,date_interval_create_from_date_string("30 days")); 
+			$thirty_days_before_date=date_format($thirty_days_before_date,"Y-m-d");
+			
+			$customer_id=$_SESSION['login_user'];
+			require_once('Database Connection file/mysqli_connect.php');
+			$query="SELECT pnr,date_of_reservation,flight_no,journey_date,class,booking_status,no_of_passengers,payment_id FROM Ticket_Details where customer_id=? AND journey_date>=? AND booking_status='CONFIRMED' ORDER BY  journey_date";
+			$stmt=mysqli_prepare($dbc,$query);
+			mysqli_stmt_bind_param($stmt,"ss",$customer_id,$todays_date);
+			mysqli_stmt_execute($stmt);
+			mysqli_stmt_bind_result($stmt,$pnr,$date_of_reservation,$flight_no,$journey_date,$class,$booking_status,$no_of_passengers,$payment_id);
+			mysqli_stmt_store_result($stmt);
+			if(mysqli_stmt_num_rows($stmt)==0)
+			{
+				echo "<h3><center>No upcoming trips!</center></h3>";
+			}
+			else
+			{
+				echo "<table cellpadding=\"10\"";
+				echo "<tr><th>PNR</th>
+				<th>Date of Reservation</th>
+				<th>Flight No.</th>
+				<th>Journey Date</th>
+				<th>Class</th>
+				<th>Booking Status</th>
+				<th>No. of Passengers</th>
+				<th>Payment ID</th>
+				</tr>";
+				while(mysqli_stmt_fetch($stmt)) {
+        			echo "<tr>
+        			<td>".$pnr."</td>
+        			<td>".$date_of_reservation."</td>
+					<td>".$flight_no."</td>
+					<td>".$journey_date."</td>
+					<td>".$class."</td>
+					<td>".$booking_status."</td>
+					<td>".$no_of_passengers."</td>
+					<td>".$payment_id."</td>
+        			</tr>";
+    			}
+    			echo "</table> <br>";
+			}
+			/*echo "<br><h3 class=\"set_nice_size\"><center><u>Completed Trips</u></center></h3>";
+
+			$query="SELECT pnr,date_of_reservation,flight_no,journey_date,class,booking_status,no_of_passengers,payment_id FROM Ticket_Details where customer_id=? and journey_date<? and journey_date>=? ORDER BY  journey_date";
+			$stmt=mysqli_prepare($dbc,$query);
+			mysqli_stmt_bind_param($stmt,"sss",$customer_id,$todays_date,$thirty_days_before_date);
+			mysqli_stmt_execute($stmt);
+			mysqli_stmt_bind_result($stmt,$pnr,$date_of_reservation,$flight_no,$journey_date,$class,$booking_status,$no_of_passengers,$payment_id);
+			mysqli_stmt_store_result($stmt);
+			if(mysqli_stmt_num_rows($stmt)==0)
+			{
+				echo "<h3><center>No trips completed in the past 30 days!</center></h3>";
+			}
+			else
+			{
+				echo "<table cellpadding=\"10\"";
+				echo "<tr><th>PNR</th>
+				<th>Date of Reservation</th>
+				<th>Flight No.</th>
+				<th>Journey Date</th>
+				<th>Class</th>
+				<th>Booking Status</th>
+				<th>No. of Passengers</th>
+				<th>Payment ID</th>
+				</tr>";
+				while(mysqli_stmt_fetch($stmt)) {
+        			echo "<tr>
+        			<td>".$pnr."</td>
+        			<td>".$date_of_reservation."</td>
+					<td>".$flight_no."</td>
+					<td>".$journey_date."</td>
+					<td>".$class."</td>
+					<td>".$booking_status."</td>
+					<td>".$no_of_passengers."</td>
+					<td>".$payment_id."</td>
+        			</tr>";
+    			}
+    			echo "</table> <br>";
+			}*/
+			mysqli_stmt_close($stmt);
+			mysqli_close($dbc);
+		?>
+		</section>
+		</div>
+		</div>
+		<!--Following data fields were empty!
+			...
+			
+			ADD VIEW FLIGHT DETAILS AND VIEW JETS/ASSETS DETAILS for ADMIN
+			PREDEFINED LOCATION WHEN BOOKING TICKETS
+
+		-->
+	</body>
+</html>
---
+++ home_page.php
@@ -0,0 +1,66 @@
+<?php
+	session_start();
+?>
+<html>
+	<head>
+		<title>
+			Welcome to Pride Airlines
+		</title>
+		<link rel="stylesheet" type="text/css" href="css/style.css"/>
+		<link rel="stylesheet" href="font-awesome-4.7.0\css\font-awesome.min.css">
+	</head>
+	<body>
+		<center>
+			<img class="logo" src="images/shutterstock_22.jpg"/> 
+			<h1 id="title">
+				GOGTE BCA AIRLINES
+			</h1>
+		</center>
+		<div>
+			<ul>
+				<li><a href="home_page.php"><i class="fa fa-home" aria-hidden="true"></i> Home</a></li>
+				<li>
+					<?php
+						if(isset($_SESSION['login_user'])&&$_SESSION['user_type']=='Customer')
+						{
+							echo "<a href=\"book_tickets.php\"><i class=\"fa fa-ticket\" aria-hidden=\"true\"></i> Book Tickets</a>";
+						}
+						else if(isset($_SESSION['login_user'])&&$_SESSION['user_type']=='Administrator')
+						{
+							echo "<a href=\"admin_ticket_message.php\"><i class=\"fa fa-ticket\" aria-hidden=\"true\"></i> Book Tickets</a>";
+						}
+						else
+						{
+							echo "<a href=\"login_page.php\"><i class=\"fa fa-ticket\" aria-hidden=\"true\"></i> Book Tickets</a>";
+						}
+					?>
+				</li>
+				<li><a href="home_page.php"><i class="fa fa-plane" aria-hidden="true"></i> About Us</a></li>
+				<li><a href="home_page.php"><i class="fa fa-phone" aria-hidden="true"></i> Contact Us</a></li>
+				<li>
+					<?php
+						if(isset($_SESSION['login_user'])&&$_SESSION['user_type']=='Customer')
+						{
+							echo "<a href=\"customer_homepage.php\"><i class=\"fa fa-sign-in\" aria-hidden=\"true\"></i> Login</a>";
+						}
+						else if(isset($_SESSION['login_user'])&&$_SESSION['user_type']=='Administrator')
+						{
+							echo "<a href=\"admin_homepage.php\"><i class=\"fa fa-sign-in\" aria-hidden=\"true\"></i> Login</a>";
+						}
+						else
+						{
+							echo "<a href=\"login_page.php\"><i class=\"fa fa-sign-in\" aria-hidden=\"true\"></i> Login</a>";
+						}
+					?>
+				</li>
+			</ul>
+		</div>
+		<div class="">
+			<img src="images/shutterstock_1.jpg" width="100%" height="600px">
+		</div>
+		<!--check out addling local host in links and other places
+
+			shift login/logout buttons to right side
+		-->
+	</body>
+</html>
